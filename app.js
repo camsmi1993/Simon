84d268bb3c63e712dcd57d3ee6bf118c8a75796f
camsmi1993 <46687363+camsmi1993@users.noreply.github.com>
2019-01-29 13:13:47 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let order = [];
+let playerOrder= [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalid;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = doucument.querySelector("#topleft");
+const topRight = doucument.querySelector("#topright");
+const bottomRight = doucument.querySelector("#bottomleft");
+const bottomLeft = doucument.querySelector("#bottomright");
+const strictButton = document.querySelector("#strict");
+const onButton= document.querySelector("#on");
+const startButton= document.querySelector("#start");
+
+
